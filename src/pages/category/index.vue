5235de81a8333af8d2e8c7e5ac6705913db3965b
autumnfish <517729329@qq.com>
2018-10-29 10:52:49 +0800

    项目初始化

---
+++ src/pages/category/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>分类</div>
+</template>
+
+<script>
+
+
+export default {
+  data () {
+    return {
+   
+    }
+  },
+
+
+  methods: {
+    
+  },
+
+  created () {
+   
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/mine/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>我的</div>
+</template>
+
+<script>
+
+
+export default {
+  data () {
+    return {
+   
+    }
+  },
+
+
+  methods: {
+    
+  },
+
+  created () {
+   
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
